324023c7134deb33cca59989639796c9ad945537
Shehryar <shehryarbitsnio@gmail.com>
2024-04-30 17:01:26 +0500

    update

--- src/Console/MakeMigrations.php
+++ src/Console/MakeMigrations.php
@@ -6,13 +6,13 @@ use Illuminate\Console\Command;
 use Bitsnio\JsonToLaravelMigrations\JsonToMigration;
 
 class MakeMigrations extends Command {
-    protected $signature = 'json:migrate {file} {--path=}';
+    protected $signature = 'json:migrate {file}';
 
     protected $description = "Create migrations from JSON file.";
 
     public function handle() {
         $this->info("Creating migrations...");
-        new JsonToMigration($this->argument('path'));
+        new JsonToMigration($this->argument('file'));
         $this->info("Migrations created!");
     }
 
